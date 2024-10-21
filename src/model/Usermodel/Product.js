197c8e503cdc80fdd543a0301c1341dd3c5a5790
Zane <hohangiang203@gmail.com>
2024-10-21 19:36:51 +0700

    update model

--- src/model/Usermodel/Product.js
+++ src/model/Usermodel/Product.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -75,6 +74,17 @@ const productSchema = new Schema({
     type: Number,
     min: 0
   },
+  sold: { 
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  rating: { 
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0
+  }
 }, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
